e48dbb32c94f6a30f318c0d2484867bdb480d605
laurenkim <lkim143@uw.edu>
2020-08-21 10:50:21 -0700

    Added rectangle overlay

--- src/components/ScreenCaptureView.tsx
+++ src/components/ScreenCaptureView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { ScreenCaptureStore } from '../stores/ScreenCaptureStore';
 
 interface ScreenCaptureViewProps {
@@ -7,8 +7,30 @@ interface ScreenCaptureViewProps {
 }
 
 export const ScreenCaptureView: FunctionComponent<ScreenCaptureViewProps> = (props) => {
+    const [rectangleWidth, setWidth] = useState(280);
+    const [rectangleHeight, setHeight] = useState(150);
+
+    const [rectX, setRectX] = useState(0);
+    const [rectY, setRectY] = useState(0);
+
+    const onMouseClick = (e: any) => {
+
+        setRectX(e.nativeEvent.offsetX);
+        setRectY(e.nativeEvent.offsetY);
+
+        setWidth(280);
+        setHeight(150);
+
+    }
+
+    
+
     return (
-        <img src={props.screenCap.screenshot} width={300} /> // < for hardcoded testing with imported screenshots //<img src={require(props.screenCap.imagePath)} />
+        <div style={{position: "relative", justifyContent:'center'}} >
+            
+            <img onClick={onMouseClick} src={props.screenCap.screenshot} style={{width: 300,  zIndex: 1}} />
+            <div style={{position: "absolute", zIndex: 2, width: rectangleWidth, height: rectangleHeight, left: rectX, top: rectY, border:"solid black 5px"}}></div>
+        </div>
     );
 
 }
